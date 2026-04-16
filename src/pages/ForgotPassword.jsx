b80f9010f0ac3b6bd8573e13c278c61d7be72b3a
navasnst <navastcheruvetty@gmail.com>
2026-04-16 12:38:52 +0530

    changed link sending button

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -3,15 +3,20 @@ import API from "../api/axios"
 
 export default function ForgotPassword() {
     const [email, setEmail] = useState("")
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        setLoading(true)  // ✅ start loading
+
         try {
             await API.post("/auth/forgot-password", { email })
-            alert("Reset link sent to your email")
+            alert("Reset link sent")
         } catch (err) {
             alert(err.response?.data?.message || "Error")
+        } finally {
+            setLoading(false)  // ✅ stop loading
         }
     }
 
@@ -27,7 +32,11 @@ export default function ForgotPassword() {
                     className="w-full mb-2 p-2 border"
                 />
 
-                <button  disabled={loading}>
+                <button
+                    disabled={loading}
+                    className={`w-full p-2 mt-2 text-white ${loading ? "bg-gray-400" : "bg-blue-500"
+                        }`}
+                >
                     {loading ? "Sending..." : "Send Reset Link"}
                 </button>
             </form>
